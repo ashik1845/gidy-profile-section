2d9894b34e0ef3454b2ed06bb4665c9570d2cf2d
Ashik <m.ashik1845@gmail.com>
2026-03-06 20:15:10 +0530

    uploads

--- server/server.js
+++ server/server.js
@@ -9,8 +9,20 @@ connectDB();
 
 const app = express();
 
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://gidy-profile-replica.netlify.app"
+];
+
 app.use(cors({
-  origin: "https://gidy-profile-replica.netlify.app",
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
